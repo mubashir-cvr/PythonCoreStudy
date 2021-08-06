f8b862c6f5b47fbedf7398634ed296e3120b654a
mubashir-cvr <mubashircheruvayoor@gmail.com>
2021-08-06 23:37:18 +0530

    String.count

--- string.py
+++ string.py
@@ -1,4 +1,9 @@
 # String Reverse 
 
-string = 'Hello World'
-print(string[::-1])
+s = 'Hello World'
+print(s[::-1])
+
+#String methods
+if s.count('l'):
+    print('Found !')
+    print('Number of ls :' + str(s.count('l')))

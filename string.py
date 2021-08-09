d21710a297c595df7518f47c21e435913f577312
mubashir-cvr <mubashircheruvayoor@gmail.com>
2021-08-09 11:57:15 +0530

    String format f string

--- string.py
+++ string.py
@@ -1,20 +1,28 @@
 # String Reverse 
 
-# s = 'Hello World'
-# print(s.capitalize())
-# print(s[::-1])
+s = 'Hello World'
+print(s.capitalize())
+print(s[::-1])
 
-# #String methods
-# if s.count('l'):
-#     print('Found !')
-#     print('Number of ls :' + str(s.count('l')))
+#String methods
+if s.count('l'):
+    print('Found !')
+    print('Number of ls :' + str(s.count('l')))
 
 #String Format 
-s= 'hi {0},Welcome to {1} programming'
-print(s.format('User','Python'))
+# s= 'hi {0},Welcome to {1} programming'
+# print(s.format('User','Python'))
 
-s= 'hi {},Welcome to {} programming'
-print(s.format('User','Python'))
+# s= 'hi {},Welcome to {} programming'
+# print(s.format('User','Python'))
 
-s= 'hi {name},Welcome to {lang} programming'
-print(s.format(name='User',lang='Python'))
+# s= 'hi {name},Welcome to {lang} programming'
+# print(s.format(name='User',lang='Python'))
+
+#String Format usinf f
+
+name = 'Mubashir'
+age = 22
+s= f'Hello {name} , Age : {age}'
+
+print(s)

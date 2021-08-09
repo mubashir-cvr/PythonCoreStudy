366ed2d6247df2a51f6429a9d942801cccb33805
mubashir-cvr <mubashircheruvayoor@gmail.com>
2021-08-09 12:28:30 +0530

    Added string Method

--- string.py
+++ string.py
@@ -5,19 +5,20 @@ print(s.capitalize())
 print(s[::-1])
 
 #String methods
-if s.count('l'):
-    print('Found !')
-    print('Number of ls :' + str(s.count('l')))
-
+print('Number of ls :' + str(s.count('l',0,10)))  
+# count ('substr',startIndex,EndIndex)
+print(f'Input in lower case : {s.lower()}')
+print(f'Input in Upper case : {s.upper()}')
+print(f'Input in Swap case : {s.swapcase()}')
 #String Format 
-# s= 'hi {0},Welcome to {1} programming'
-# print(s.format('User','Python'))
+s= 'hi {0},Welcome to {1} programming'
+print(s.format('User','Python'))
 
-# s= 'hi {},Welcome to {} programming'
-# print(s.format('User','Python'))
+s= 'hi {},Welcome to {} programming'
+print(s.format('User','Python'))
 
-# s= 'hi {name},Welcome to {lang} programming'
-# print(s.format(name='User',lang='Python'))
+s= 'hi {name},Welcome to {lang} programming'
+print(s.format(name='User',lang='Python'))
 
 #String Format usinf f string 
 

daa7dcda94b676abaceb76bd0db5d6559575d9cd
mubashir-cvr <mubashircheruvayoor@gmail.com>
2021-08-11 11:07:42 +0530

    Test Function

--- Assignments.py
+++ Assignments.py
@@ -3,11 +3,18 @@
 #     print(f'{i} x 2 = {i*2}')
 
 
-InPut=input('Enter Your input :')
-if InPut.isnumeric():
-    print(f' You Entered a Numeric  = {int(InPut)} ')
-else:
-    print('Input in lower case : ',InPut.lower())
-    print(f'Input in Upper case : {InPut.upper()}')
-    print(f'Input in Swap case : {InPut.swapcase()}')
-    print(f'I44nput in Capitalized  : {InPut.capitalize()}')
+# InPut=input('Enter Your input :')
+# if InPut.isnumeric():
+#     print(f' You Entered a Numeric  = {int(InPut)} ')
+# else:
+#     print('Input in lower case : ',InPut.lower())
+#     print(f'Input in Upper case : {InPut.upper()}')
+#     print(f'Input in Swap case : {InPut.swapcase()}')
+#     print(f'I44nput in Capitalized  : {InPut.capitalize()}')
+
+
+def myfn(**data):
+    print(data)
+    print(type(data['key1']))
+
+myfn(key1='name',key2=2.3,Key3=3,key4=4,key5=5)

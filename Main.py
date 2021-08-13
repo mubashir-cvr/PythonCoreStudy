8bad5a86b6629ed5e006f37a6788b68b4eaf250a
mubashir-cvr <mubashircheruvayoor@gmail.com>
2021-08-13 11:32:23 +0530

    Initiate package with __init__.py

--- Main.py
+++ Main.py
@@ -1,10 +1,18 @@
-from MyPackage.pack1 import display as disp1
+# from MyPackage.pack1 import display as disp1
 from MyPackage.pack2 import display as disp2
 from MyPackage import pack1 as firstpack
 import MyPackage as customPackage
 import MyPackage
-disp1()
 disp2()
 firstpack.display()
 customPackage.pack2.display()
 MyPackage.pack1.display()
+
+
+print("\n from __init__.py")
+# Configured from __init__.py in package 
+import MyPackage as m
+m.disp1()
+m.disp2()
+m.display()  # function Over load 
+
---
+++ MyPackage/__init__.py
@@ -0,0 +1,4 @@
+from MyPackage.pack1 import display as disp1
+from MyPackage.pack2 import display as disp2
+from MyPackage.pack1 import display
+from MyPackage.pack2 import display 

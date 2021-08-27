82681c2f22fcbd52a288114f5ef20f31470faf18
mubashir-cvr <mubashircheruvayoor@gmail.com>
2021-08-27 13:44:19 +0530

    Comprehension

--- List_Comprehension.py
+++ List_Comprehension.py
@@ -1,5 +1,31 @@
-fruits = ["apple", "banana", "cherry", "kiwi", "mango"]
+# fruits = ["apple", "banana", "cherry", "kiwi", "mango"]
 
-newlist = [x for x in fruits if "a" in x]
+# newlist = [x for x in fruits if "a" in x]
+
+# print(newlist)
+
+items=[]
+inputs=["item","price","quantity"]
+numbofitem=int(input('Number of Item :'))
+for i in range(numbofitem):
+    item={}
+    for attr in inputs:
+        item[attr]=input(f'{attr.capitalize()}  :')
+    items.append(item)
+print('Items')
+print('#'*50)
+print(items)
+
+#List Comprehension
+newlist = [x for x in items if 'apple' in x['item'].lower()] 
+print('Filtered Items (Apple)')
+print('#'*50)
 
 print(newlist)
+keys=('item','price')
+print('Filtered Item price')
+print('#'*50)
+for item in newlist:
+    # Dict Comprehension
+    item_and_price ={x:item[x] for x in keys} 
+    print(item_and_price)

7bc43f46e7c5ae5a41fc74ad7bb0c3c0f1045b7b
mubashir-cvr <mubashircheruvayoor@gmail.com>
2021-08-30 11:38:49 +0530

    Simplifying Comprehension

--- List_Comprehension.py
+++ List_Comprehension.py
@@ -4,28 +4,42 @@
 
 # print(newlist)
 
-items=[]
-inputs=["item","price","quantity"]
-numbofitem=int(input('Number of Item :'))
-for i in range(numbofitem):
-    item={}
-    for attr in inputs:
-        item[attr]=input(f'{attr.capitalize()}  :')
-    items.append(item)
-print('Items')
-print('#'*50)
-print(items)
+# items=[]
+# inputs=["item","price","quantity"]
+# numbofitem=int(input('Number of Item :'))
+# for i in range(numbofitem):
+#     item={}
+#     for attr in inputs:
+#         item[attr]=input(f'{attr.capitalize()}  :')
+#     items.append(item)
+# print('Items')
+# print('#'*50)
+# print(items)
 
-#List Comprehension
-newlist = [x for x in items if 'apple' in x['item'].lower()] 
-print('Filtered Items (Apple)')
-print('#'*50)
+# #List Comprehension
+# newlist = [x for x in items if 'apple' in x['item'].lower()] 
+# print('Filtered Items (Apple)')
+# print('#'*50)
 
-print(newlist)
-keys=('item','price')
-print('Filtered Item price')
-print('#'*50)
-for item in newlist:
-    # Dict Comprehension
-    item_and_price ={x:item[x] for x in keys} 
-    print(item_and_price)
+# print(newlist)
+# keys=('item','price')
+# print('Filtered Item price')
+# print('#'*50)
+# for item in newlist:
+#     # Dict Comprehension
+#     item_and_price ={x:item[x] for x in keys} 
+#     print(item_and_price)
+
+
+
+
+#########################################
+
+lst =['Red','Green','Blue']
+_dst={k:[input('%s :'%v) for v in lst] for k in range(2)}
+
+print(_dst)
+
+_lst = {v for k,v in _dst.items() }
+
+print(_lst)
